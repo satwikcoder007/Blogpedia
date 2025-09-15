91a9193b794c48f6e522091432b00bda2b190029
Ryzen_10 <115201346+Dipu010@users.noreply.github.com>
2025-09-15 15:11:24 +0530

    Added API Gateway added logging featue in blog and profile service changed profile route to /profile

---
+++ profileServer/config/logger.js
@@ -0,0 +1,29 @@
+import { createLogger, format, transports } from 'winston';
+import moment from 'moment-timezone';
+
+
+const customFormat = format.printf(({ timestamp, level, message, service }) => {
+  const tag = service ? `[${service.toUpperCase()}]` : '[UNKNOWN]';
+  const istTs = moment(timestamp).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+  return `${istTs} ${level.toUpperCase()} ${tag}: ${message}`;
+});
+
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    customFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: '/shared-logs/app.log', level: 'info' }),
+    //new transports.File({ filename: '/shared-logs/app-error.log', level: 'error' })
+  ],
+});
+
+
+export const getServiceLogger = (serviceName) =>
+  logger.child({ service: serviceName });
+
+export default logger;
