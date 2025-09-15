ec5e749aca1f201f69af246035852e7b7e15aa18
Pratip2004 <157035698+Pratip2004@users.noreply.github.com>
2025-09-16 02:35:25 +0530

    TagQueue added and tag updated in db

--- llmTags/tagcontentQueue.js
+++ llmTags/tagcontentQueue.js
@@ -3,27 +3,41 @@ import { Blog } from "./models/Blog.js";
 import {generateTags} from "./genTag.js"
 
 export async function consumeQueue() {
-    const conn=await amqp.connect(process.env.AMQP_URL)
-    const channel = await conn.createChannel();
-    await channel.assertQueue("tagQueue");
+  const conn = await amqp.connect(process.env.AMQP_URL);
+  const channel = await conn.createChannel();
+  await channel.assertQueue("tagQueue");
 
-    channel.consume("tagQueue", async(msg)=>{
-        if(msg!=null){
-            const blog_id=JSON.parse(msg.content.toString())
-            const blog=await Blog.find({_id:blog_id})
-            // generating the tags
+  channel.consume("tagQueue", async (msg) => {
+    if (msg) {
+      try {
+        // Parse blog id
+        const blog_id = JSON.parse(msg.content.toString());
 
-            const title=blog?.title
-            const content=blog?.content
-
-            const tags=generateTags(title,content)
-
-            await Blog.findByIdAndUpdate(blog_id,
-                { $push: { tags: { $each: tags } } },
-                {new:true}
-            )
-
-            channel.ack(msg);
+        // Find blog by ID (returns a single doc)
+        const blog = await Blog.findById(blog_id);
+        if (!blog) {
+          console.error(`Blog ${blog_id} not found`);
+          channel.ack(msg);
+          return;
         }
-    })
+
+        // Generate tags using the blog’s content/title
+        const tags = await generateTags(blog.title, blog.content);
+        //console.log("Generated tags:", tags);
+
+        // Update the blog
+        await Blog.findByIdAndUpdate(
+          blog._id,
+          { $set: { tags } },
+          { new: true }
+        );
+
+        //console.log(`Tags updated for blog ${blog._id}`);
+        channel.ack(msg);
+      } catch (err) {
+        console.error("Error consuming message:", err);
+        channel.nack(msg, false, false); // reject without requeue
+      }
+    }
+  });
 }
